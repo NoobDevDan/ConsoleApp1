f87397cacb475719fb581f3b9d471784a40f7c6b
NoobDevDan <dan_lelievre@hotmail.com>
2017-05-10 11:29:08 +1000

    First Git Save

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static List<int> numList = new List<int>();
+
+        static void Main(string[] args)
+        {
+            for(int i =0; i < 10; i++)
+            {
+                numList.Add(i);
+            }
+
+            int[] array = numList.ToArray();
+
+            foreach(int i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
